10e2e84b2b7799a98e47412f4ae98af19270da9d
Christopher Appiah-Thompson <chrsappiah@gmail.com>
2026-04-14 16:00:38 +1000

    docs: add proprietary license and tighten source headers

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,2 +1,1 @@
-// Copyright (c) 2026 World Class Scholars, led by Dr Christopher Appiah-Thompson. All rights reserved.
 module.exports = { content: ['./index.html', './src/**/*.{jsx,tsx}'], theme: { extend: {} }, plugins: [] }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,3 @@
-// Copyright (c) 2026 World Class Scholars, led by Dr Christopher Appiah-Thompson. All rights reserved.
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({ plugins: [react()], server: { proxy: { '/api': 'http://localhost:3001' } } });
